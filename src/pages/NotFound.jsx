e8995e486ac01fe39533361755d4f70e7aa2426a
Mirjalol9080 <pubgmabile8862@gmail.com>
2025-04-21 01:31:27 +0500

    M

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -4,8 +4,8 @@ function NotFound() {
   return (
     <div className='notfound'>
         <div className="container">
-            <h1 className="nothound_title">404 - Sahifa topilmadi</h1>
-            <p className="nothound_title">Bunday sahifa mavjud emas.</p>
+            <h1 className="notfound_title">404 - Sahifa topilmadi</h1>
+            <p className="notfound_subtitle">Bunday sahifa mavjud emas.</p>
         </div>
     </div>
   )
